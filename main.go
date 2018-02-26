b7527f4afd6b0e315c139cbffc549665386883b9
vaerjngiar <vaerjngiar@gmail.com>
2018-02-26 15:51:12 +0200

    initial

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	phonedb "phonenormalizer/db"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "go_user"
+	password = "password"
+	dbname   = "gophercises_phone"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	must(phonedb.Reset("postgres", psqlInfo, dbname))
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	must(phonedb.Migrate("postgres", psqlInfo))
+
+	db, err := phonedb.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	err = db.Seed()
+	must(err)
+
+	phones, err := db.AllPhones()
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("Working on... %+v\n", p)
+		number := normalize(p.Number)
+		if number != p.Number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := db.FindPhone(number)
+			must(err)
+			if existing != nil {
+				must(db.DeletePhone(p.ID))
+			} else {
+				p.Number = number
+				must(db.UpdatePhone(&p))
+			}
+		} else {
+			fmt.Println("No changes required")
+		}
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func normalize(phone string) string {
+	re := regexp.MustCompile("\\D")
+	return re.ReplaceAllString(phone, "")
+}
+
+// func normalize(phone string) string {
+// 	var buf bytes.Buffer
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			buf.WriteRune(ch)
+// 		}
+// 	}
+// 	return buf.String()
+// }
